3979f1015017bbbc600b5902e14b132cb703d0d0
tnct3000@gmail.com <tnct3000@gmail.com>
2024-10-28 15:00:04 +0700

    thêm admin và các phần để làm thêm trong body

---
+++ thiet ke trang/Areas/Admin/Controllers/Home1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace thiet_ke_trang.Areas.Admin.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
